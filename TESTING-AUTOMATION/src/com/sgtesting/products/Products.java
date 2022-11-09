51d9d4e97c5c529e026766e31ca18080ac4e111a
smmpower <manoharbm2@gmail.com>
2022-11-08 23:54:45 -0800

    Testng

---
+++ TESTING-AUTOMATION/src/com/sgtesting/products/Products.java
@@ -0,0 +1,39 @@
+package com.sgtesting.products;
+
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
+
+public class Products {
+
+	@Test
+	public void createProduct()
+	{
+		System.out.println("User demoProduct1 has created successfully...");
+	}
+	
+	@Test
+	public void modifyProduct()
+	{
+		System.out.println("User demoProduct1 has modified successfully...");
+	}
+
+	@Test
+	public void deleteProduct()
+	{
+		System.out.println("User demoProduct1 has deleted successfully...");
+	}
+	
+	@BeforeClass
+	public void setUp()
+	{
+		System.out.println("Launch Browser, Navigate URL and Login into the Application");
+	}
+	
+	@AfterClass
+	public void tearDown()
+	{
+		System.out.println("Logout from the Application and Close Application..");
+		System.out.println("+++++++++++++++++++++++++++++");
+}
+}
---
+++ TESTING-AUTOMATION/src/com/sgtesting/softassertion/AssertNullDemo.java
@@ -0,0 +1,42 @@
+package com.sgtesting.softassertion;
+
+import java.io.File;
+
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+public class AssertNullDemo {
+	
+	@Test
+	public void assertNullDemo()
+	{
+		try
+		{
+			SoftAssert obj=new SoftAssert();
+			File f1=null;
+			obj.assertNull(f1);
+			System.out.println("The assertNull Step has executed properly");
+			obj.assertAll();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+
+	@Test
+	public void assertNotNullDemo()
+	{
+		try
+		{
+			SoftAssert obj=new SoftAssert();
+			File f1=null;
+			f1=new File("D:\\EXAMPLE");
+			obj.assertNotNull(f1);
+			System.out.println("The assertNotNull Step has executed properly");
+			obj.assertAll();
+		}catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
